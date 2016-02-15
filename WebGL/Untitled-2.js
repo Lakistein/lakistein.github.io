239cb9e4a76593b3f4600b13ea873046b07a60b5
Lakistein <lazarlazarevic@msn.com>
2016-02-15 12:17:16 +0100

    f

--- WebGL/Untitled-2.js
+++ WebGL/Untitled-2.js
@@ -1,43 +1,19 @@
 /// <reference path="babylon.2.3.d.ts" />
-/// <reference path="babylon.objFileLoader.ts" />
-/// <reference path="babylon.pbrMaterial.min.js" />
+/// <reference path="babylon.pbrMaterial.js" />
 /// <reference path="babylon.pbrMaterial.d.ts" />
-// get the canvas DOM element
- // get the canvas DOM element
-    var canvas = document.getElementById('renderCanvas');
 
-    // load the 3D engine
+    var canvas = document.getElementById('renderCanvas');
     var engine = new BABYLON.Engine(canvas, true);
 
-    // createScene function that creates and return the scene
     var createScene = function(){
-        // create a basic BJS Scene object
         var scene = new BABYLON.Scene(engine);
-
-        // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
         var camera = new BABYLON.ArcRotateCamera("ArcRotateCamera", 1, 0.8, 10, new BABYLON.Vector3(0, 0, 0), scene);
-
-
-
-        // target the camera to scene origin
-
-        // attach the camera to the canvas
         camera.attachControl(canvas, false);
-
-        // create a basic light, aiming 0,1,0 - meaning, to the sky
-        var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
-        //light.groundColor = new BABYLON.Color3(255, 255, 255);
-       // light.intensity = 0.01;
-        // create a built-in "sphere" shape; its constructor takes 5 params: name, width, depth, subdivisions, scene
-        //var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
-
-        // move the sphere upward 1/2 of its height
-        //sphere.position.y = 1;
-
-        // create a built-in "ground" shape; its constructor takes the same 5 params as the sphere's one
-        //var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
-
-        // return the created scene    
+        camera.wheelPrecision = 20;
+        //var light = new BABYLON.PointLight('light', new BABYLON.Vector3(0,1,-3), scene);  
+        var light1 = new BABYLON.PointLight('light1', new BABYLON.Vector3(0,3,-3), scene);  
+        light1.intensity = 2;
+        
         BABYLON.SceneLoader.ImportMesh("", "./", "HEADSET.babylon", scene, function (newMeshes) {
 
             var redPlastic = new BABYLON.StandardMaterial("plastic", scene);
@@ -46,36 +22,56 @@
             var chrome = new BABYLON.StandardMaterial("chrome", scene);
             var blackPlastic = new BABYLON.StandardMaterial("blackPlastic", scene);
             var blackMetal = new BABYLON.StandardMaterial("blackMetal", scene);
+            var HeadsetAO = new BABYLON.Texture("./HEADSET_STYLE_1.jpg", scene);
             
-            redPlastic.diffuseTexture = new BABYLON.Texture("./redplastic.jpg",scene);        
-            blackBox.diffuseTexture = new BABYLON.Texture("./blackbox.jpg",scene);
+            redPlastic.diffuseTexture = new BABYLON.Texture("./redplastic.jpg",scene);   
+            redPlastic.ambientTexture =  HeadsetAO;
+            //redPlastic.ambientTexture.coordinatesIndex = 1;
+            
+            blackBox.diffuseTexture = new BABYLON.Texture("./blackbox.jpg", scene);
+            blackBox.ambientTexture = new BABYLON.Texture("./BOX_STYLE_1.jpg", scene);
+            blackBox.ambientTexture.coordinatesIndex = 1;
+            
             blackCushion.diffuseTexture = new BABYLON.Texture("./blackcushion.jpg",scene);
+            blackCushion.ambientTexture =  HeadsetAO;
+            blackCushion.ambientTexture.coordinatesIndex = 1;
+                        
             chrome.diffuseTexture = new BABYLON.Texture("./chrome.jpg",scene);
+            chrome.ambientTexture =  HeadsetAO;
+            chrome.ambientTexture.coordinatesIndex = 1;
+                                    
             blackPlastic.diffuseTexture = new BABYLON.Texture("./blackplastic.jpg",scene);
+            blackPlastic.ambientTexture =  HeadsetAO;
+            blackPlastic.ambientTexture.coordinatesIndex = 1;
+                                    
             blackMetal.diffuseTexture = new BABYLON.Texture("./blackmetal.jpg",scene);          
-              
-            newMeshes[0].material = chrome;
-            newMeshes[1].material = blackMetal;
-            newMeshes[4].material = blackPlastic;
-            newMeshes[2].material = redPlastic;
-            newMeshes[6].material = blackBox;
-            newMeshes[3].material = blackCushion;
+            blackMetal.ambientTexture =  HeadsetAO;
+            blackMetal.ambientTexture.coordinatesIndex = 1;
+                                    
+            for (var i = 0; i < newMeshes.length; i++) {
+                switch (newMeshes[i].name) {
+                    case "BOX_STYLE_1": newMeshes[i].material = blackBox;  break;
+                    case "HEADSETARCH_STYLE_1": newMeshes[i].material = blackMetal; break;
+                    case "HEADSETBLACKPLASTIC_STYLE_1": newMeshes[i].material = blackPlastic; break;
+                    case "HEADSETCHROME_STYLE_1": newMeshes[i].material = chrome; break;
+                    case "HEADSETCOLOREDPLASTIC_STYLE_1": newMeshes[i].material = redPlastic; break;
+                    case "HEADSETCUSHION_STYLE_1": newMeshes[i].material = blackCushion; break;
+                    default: break;
+                }                
+            }
+
             camera.target = newMeshes[0];
 
     	});
         return scene;
     }
 
-    // call the createScene function
     var scene = createScene();
 
-    // run the render loop
     engine.runRenderLoop(function(){
         scene.render();
     });
 
-    // the canvas/window resize event handler
     window.addEventListener('resize', function(){
         engine.resize();
     });
- 
