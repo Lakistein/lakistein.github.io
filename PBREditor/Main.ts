63770eb1ff7e29b3507ef5ee76e1f60b18d0cef9
Lakistein <lazarlazarevic@msn.com>
2016-07-28 20:48:53 +0200

    a

--- PBREditor/Main.ts
+++ PBREditor/Main.ts
@@ -233,6 +233,8 @@ window.addEventListener('DOMContentLoaded', function () {
         hdrSkyboxMaterial.microSurface = 0.85;
         hdrSkyboxMaterial.cameraExposure = 0.6;
         hdrSkyboxMaterial.cameraContrast = 1.6;
+        hdrSkyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+
         //hdrSkyboxMaterial.disableLighting = true;
         hdrSkybox.material = hdrSkyboxMaterial;
         hdrSkybox.infiniteDistance = true;
@@ -244,7 +246,7 @@ window.addEventListener('DOMContentLoaded', function () {
             Sphere.material = Sphere_Pbr;
             Sphere.position.addInPlace(new BABYLON.Vector3(8, 0, 0));
             displayMaterialValues(Sphere_Pbr, scene);
-            Sphere_Pbr.reflectionTexture = hdrTexture;
+            Sphere_Pbr.reflectionTexture = hdrTexture.clone();
             Sphere_Pbr.refractionTexture = hdrTexture;
             meshes.push(Sphere);
         }
@@ -256,7 +258,7 @@ window.addEventListener('DOMContentLoaded', function () {
             Cube.material = Cube_pbr;
             Cube.position.addInPlace(new BABYLON.Vector3(0, 0, 0));
             displayMaterialValues(Cube_pbr, scene);
-            Cube_pbr.reflectionTexture = hdrTexture;
+            Cube_pbr.reflectionTexture = hdrTexture.clone();
             Cube_pbr.refractionTexture = hdrTexture;
             meshes.push(Cube);
         }
@@ -269,7 +271,7 @@ window.addEventListener('DOMContentLoaded', function () {
             Plane.position.addInPlace(new BABYLON.Vector3(-8, 0, 0));
             Plane.rotate(new BABYLON.Vector3(1, 0, 0), 1.5708);
             displayMaterialValues(PLane_pbr, scene);
-            PLane_pbr.reflectionTexture = hdrTexture;
+            PLane_pbr.reflectionTexture = hdrTexture.clone();
             PLane_pbr.refractionTexture = hdrTexture;
             meshes.push(Plane);
         }
